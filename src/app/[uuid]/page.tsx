668f27f4f23fc735b3d3049b31ca61792d2140e1
lucasfvera <lucasfvera@hotmail.com>
2025-06-22 13:19:55 -0300

    Add cta for 404 page

--- src/app/[uuid]/page.tsx
+++ src/app/[uuid]/page.tsx
@@ -1,4 +1,5 @@
 import { MOCKED_PAGES } from '@/helpers/mockPages';
+import Link from 'next/link';
 
 export default async function FormPage({
 	params,
@@ -10,7 +11,16 @@ export default async function FormPage({
 
 	return (
 		<div className="flex w-full bg-amber-300 h-full flex-1 rounded-lg items-center justify-center">
-			{page ? page.title : "Oops! We couldn't find the page"}
+			{page ? (
+				page.title
+			) : (
+				<div>
+					<p>{"Oops! We couldn't find the page"}</p>{' '}
+					<Link href={'/'} className="text-blue-600 hover:underline">
+						Go back home
+					</Link>
+				</div>
+			)}
 		</div>
 	);
 }
